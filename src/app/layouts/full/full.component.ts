84e10fb73de14dfe34ea4eac8c355c054a5f0553
geniusunil <geniusunil@gmail.com>
2018-12-15 12:48:05 +0530

    responsive before floating topbar right

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -1,6 +1,6 @@
-import * as $ from 'jquery';
+// import * as $ from 'jquery';
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Router } from '@angular/router';
+// import { Router } from '@angular/router';
 import {
   ChangeDetectorRef,
   Component,
@@ -9,11 +9,12 @@ import {
   ViewChild,
   HostListener,
   Directive,
-  AfterViewInit
+  AfterViewInit,
+  OnInit
 } from '@angular/core';
 import { trigger, transition, state, animate, style } from '@angular/animations';
 import { MenuItems } from '../../shared/menu-items/menu-items';
-import { AppHeaderComponent } from './header/header.component';
+// import { AppHeaderComponent } from './header/header.component';
 import { AppSidebarComponent } from './sidebar/sidebar.component';
 
 /** @title Responsive sidenav */
@@ -39,6 +40,11 @@ import { AppSidebarComponent } from './sidebar/sidebar.component';
         // opacity: 0.5,
         // backgroundColor: 'green'
       })),
+      state('fullOpened', style({
+        width: '50vw',
+        // opacity: 0.5,
+        // backgroundColor: 'green'
+      })),
       transition('* => *', [
         animate('0.5s')
       ]),
@@ -55,6 +61,16 @@ import { AppSidebarComponent } from './sidebar/sidebar.component';
         // opacity: 0.5,
         // backgroundColor: 'green'
       })),
+      state('fullClosed', style({
+        marginLeft: '0',
+        // opacity: 0.5,
+        // backgroundColor: 'green'
+      })),
+      state('fullOpened', style({
+        marginLeft: '50vw',
+        // opacity: 0.5,
+        // backgroundColor: 'green'
+      })),
       transition('* => *', [
         animate('0.5s')
       ]),
@@ -62,15 +78,17 @@ import { AppSidebarComponent } from './sidebar/sidebar.component';
     
   ],
 })
-export class FullComponent implements OnDestroy, AfterViewInit {
+export class FullComponent implements OnDestroy, AfterViewInit,OnInit {
   mobileQuery: MediaQueryList;
   widthSidebarExpanded : number = 14.5;
   widthSidebarCollapsed : number = 5.5;
   widthSidebar : number = this.widthSidebarExpanded;
   private _mobileQueryListener: () => void;
   isOpen = true;
+  isWide = false;
   // public iconsDisplay: string = AppSidebarComponent.iconsDisplayValue();
   @ViewChild(AppSidebarComponent) child;
+  innerWidth: number;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
@@ -90,6 +108,14 @@ export class FullComponent implements OnDestroy, AfterViewInit {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
   ngAfterViewInit() {}
+
+  ngOnInit(){
+    this.isWide = window.innerWidth>767;
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.isWide = window.innerWidth>767;
+  }
   sidebarToggle(){
     if(this.widthSidebar == this.widthSidebarExpanded){
       this.widthSidebar = this.widthSidebarCollapsed;
