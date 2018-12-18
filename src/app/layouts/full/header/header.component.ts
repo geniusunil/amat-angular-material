0ee82e9a9cb507fe51c984dbaeead4b3b0a48a78
geniusunil <geniusunil@gmail.com>
2018-12-18 15:34:01 +0530

    custom select done

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -1,12 +1,28 @@
 
 
 import { Component } from '@angular/core';
-
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: []
 })
+
+
 export class AppHeaderComponent {
   public toggleSearchBar:boolean = false;
+  // names = [1,2,3];
+  constructor(){
+    
+  }
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'ALL'},
+    {value: 'pizza-1', viewValue: 'Accounts'},
+    {value: 'tacos-2', viewValue: 'Contacts'},
+    {value: 'tacos-3', viewValue: 'Dashboard'}
+  ];
+  selectedValue: string = this.foods[0].value;
 }
