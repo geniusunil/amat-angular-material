de86cc19709c055d91aa366c91ffa06574c23a75
geniusunil <geniusunil@gmail.com>
2018-12-21 21:27:55 +0530

    renderer

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Directive, Renderer2, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+@Directive({
+  selector: '[appGoWild]'
+})
 export class DashboardComponent implements OnInit {
   htmlToAdd;
-  constructor() { }
+  @ViewChild('test') test: ElementRef;
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit() {
     
   }
   ngAfterViewInit(){
-    this.htmlToAdd = document.getElementById("overdue").innerHTML +"<div>hi</div>";
-    // console.log(this.htmlToAdd);
-    document.getElementById("overdue").innerHTML = this.htmlToAdd;
+    // this.renderer.addClass(this.el.nativeElement, 'wild');
+    this.renderer.addClass(this.test.nativeElement, 'yo');
+    this.renderer.addClass(this.test.nativeElement, 'yo');
+    this.renderer.setStyle(this.test.nativeElement, 'backgroundColor', 'red');
+    this.renderer.setStyle(this.test.nativeElement, 'color', 'white');
+ //https://vahrushev.info/2018/05/26/how-to-modify-third-party-element-in-angular/
+    console.log("ngafterviewinit");
   }
-
+    
+  
 }
