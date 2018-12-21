2aaa61c1bd76830d5d38628e6eca105bba990959
geniusunil <geniusunil@gmail.com>
2018-12-21 17:24:35 +0530

    dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  htmlToAdd;
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  ngAfterViewInit(){
+    this.htmlToAdd = document.getElementById("overdue").innerHTML +"<div>hi</div>";
+    // console.log(this.htmlToAdd);
+    document.getElementById("overdue").innerHTML = this.htmlToAdd;
   }
 
 }
