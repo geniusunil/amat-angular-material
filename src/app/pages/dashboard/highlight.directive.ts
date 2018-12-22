19c9885923e71b54d506e343e05f1774dbd97f1f
geniusunil <geniusunil@gmail.com>
2018-12-22 13:01:21 +0530

    renderer 1 added for the first time

--- src/app/pages/dashboard/highlight.directive.ts
+++ src/app/pages/dashboard/highlight.directive.ts
@@ -1,13 +1,22 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(el: ElementRef) {
+  constructor(el: ElementRef,private _renderer2: Renderer2) {
     console.log(el.nativeElement);
     el.nativeElement.style.backgroundColor = 'yellow';
+    const div = this._renderer2.createElement('div');
+    console.log(el.nativeElement.parentNode);
+    const matTabBodyWrapper = el.nativeElement.querySelector('.mat-tab-header');
+    const paramText = this._renderer2.createText("1");
+ 
+    this._renderer2.appendChild(div, paramText);
+    console.log(div);
+    this._renderer2.addClass(el.nativeElement, 'wild');
+    this._renderer2.appendChild(el.nativeElement, div );
  }
 
 }
