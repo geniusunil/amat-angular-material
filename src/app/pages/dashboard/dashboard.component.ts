bddc887e86463899447b4d4159e9f445d9a3e8c6
Sunil Sharma <geniusunil@gmail.com>
2018-12-22 05:38:44 +0000

    Revert "renderer"
    This reverts commit de86cc19709c055d91aa366c91ffa06574c23a75

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,30 +1,21 @@
-import { Component, OnInit,Directive, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-@Directive({
-  selector: '[appGoWild]'
-})
 export class DashboardComponent implements OnInit {
   htmlToAdd;
-  @ViewChild('test') test: ElementRef;
-  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  constructor() { }
 
   ngOnInit() {
     
   }
   ngAfterViewInit(){
-    // this.renderer.addClass(this.el.nativeElement, 'wild');
-    this.renderer.addClass(this.test.nativeElement, 'yo');
-    this.renderer.addClass(this.test.nativeElement, 'yo');
-    this.renderer.setStyle(this.test.nativeElement, 'backgroundColor', 'red');
-    this.renderer.setStyle(this.test.nativeElement, 'color', 'white');
- //https://vahrushev.info/2018/05/26/how-to-modify-third-party-element-in-angular/
-    console.log("ngafterviewinit");
+    this.htmlToAdd = document.getElementById("overdue").innerHTML +"<div>hi</div>";
+    // console.log(this.htmlToAdd);
+    document.getElementById("overdue").innerHTML = this.htmlToAdd;
   }
-    
-  
+
 }
