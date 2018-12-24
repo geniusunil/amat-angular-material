effb076f331cc9361453bfd8e196c7877ab515ab
geniusunil <geniusunil@gmail.com>
2018-12-24 16:19:59 +0530

    feather icon

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,27 @@
 import { Component, OnInit,AfterContentInit,
   Directive,
-  Renderer2 } from '@angular/core';
+  Renderer2,
+SecurityContext } from '@angular/core';
+import { FeatherIconsPipe } from '../../feather-pipe';
+
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { inject, TestBed } from '@angular/core/testing';
 //hi
+/* describe('SanitiseHtmlPipe', () => {
+  beforeEach(() => {
+    TestBed
+      .configureTestingModule({
+        imports: [
+          BrowserModule
+        ]
+      });
+  });
+
+  it('create an instance', inject([DomSanitizer], (domSanitizer: DomSanitizer) => {
+    let pipe = new FeatherIconsPipe(domSanitizer);
+    expect(pipe).toBeTruthy();
+  })); 
+}); */
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -13,8 +33,8 @@ import { Component, OnInit,AfterContentInit,
 export class DashboardComponent implements OnInit {
   htmlToAdd;
   constructor(
-    private _renderer2: Renderer2) { }
-   
+    private _renderer2: Renderer2,
+    private sanitizer:DomSanitizer) { }
    
   ngOnInit() {
     
@@ -23,15 +43,23 @@ export class DashboardComponent implements OnInit {
 
   }
   ngAfterContentInit() {
-    
+    let pipe;
      const div = this._renderer2.createElement('div');
+     const span = this._renderer2.createElement('span');
+     console.log(this.sanitizer);
+    pipe = new FeatherIconsPipe(this.sanitizer);
+   
+    
+     let name =pipe.transform('home');
+     console.log(name);
+     this._renderer2.setProperty(span, 'innerHTML', this.sanitizer.sanitize(SecurityContext.HTML,name));
     // const matTabBodyWrapper = this._el.nativeElement.parentNode.querySelector('.mat-tab-body-wrapper');
     const matTabBodyWrapper = (document.getElementById('overdue')).getElementsByClassName('mat-tab-body-wrapper')[0];
 const mat=(document.getElementById('overdue')).getElementsByClassName('mat-tab-header')[0];
 this._renderer2.setStyle(mat,'border','none');
-    const paramText = this._renderer2.createText("hi");
+    const paramText = this._renderer2.createText("");
  
-    this._renderer2.appendChild(div, paramText);
+    this._renderer2.appendChild(div, span);
  
     this._renderer2.insertBefore(matTabBodyWrapper.parentNode, div,matTabBodyWrapper);
     //  console.log(matTabBodyWrapper);
