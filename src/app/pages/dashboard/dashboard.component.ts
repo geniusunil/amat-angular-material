741bee3a70e2c028a24e2645714e00d29d5f0140
geniusunil <geniusunil@gmail.com>
2018-12-24 17:15:56 +0530

    insert

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -52,7 +52,7 @@ export class DashboardComponent implements OnInit {
      let fileMinus =this.sanitizer.sanitize(SecurityContext.HTML,pipe.transform('file-minus'));
      let pdf =this.sanitizer.sanitize(SecurityContext.HTML,pipe.transform('file-minus'));
      console.log(name);
-     let final= name + " | "+name + " Selected |" + fileMinus + " | " ;
+     let final= name + " | "+name + " Selected |" + fileMinus + " | " + fileMinus + " | Visibility";
      this._renderer2.setProperty(span, 'innerHTML', final);
     // const matTabBodyWrapper = this._el.nativeElement.parentNode.querySelector('.mat-tab-body-wrapper');
     const matTabBodyWrapper = (document.getElementById('overdue')).getElementsByClassName('mat-tab-body-wrapper')[0];
@@ -61,7 +61,7 @@ this._renderer2.setStyle(mat,'border','none');
     const paramText = this._renderer2.createText("");
  
     this._renderer2.appendChild(div, span);
- 
+    this._renderer2.addClass(div,'overdueMeetingsInsert');
     this._renderer2.insertBefore(matTabBodyWrapper.parentNode, div,matTabBodyWrapper);
     //  console.log(matTabBodyWrapper);
      this._renderer2.setStyle(matTabBodyWrapper,'border','1px solid rgba(0, 0, 0, 0.12)');
