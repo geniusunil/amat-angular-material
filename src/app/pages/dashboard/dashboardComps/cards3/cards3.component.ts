e51f4d57256cc0669502e892668e9bed94102962
geniusunil <geniusunil@gmail.com>
2019-01-02 15:21:22 +0530

    before table scrollbar

--- src/app/pages/dashboard/dashboardComps/cards3/cards3.component.ts
+++ src/app/pages/dashboard/dashboardComps/cards3/cards3.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class Cards3Component implements OnInit {
   @Input() titleicon;
+  @Input() title;
   titleicons = 'home';
   constructor() { }
 
